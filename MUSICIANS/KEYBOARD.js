67eda73fd8914019698cee72db6bd2da0f81309d
garrett <garrett.v.parrish@gmail.com>
2014-12-10 03:53:48 -0500

    Added running histories of notes with timestamps.

--- MUSICIANS/KEYBOARD.js
+++ MUSICIANS/KEYBOARD.js
@@ -10,17 +10,39 @@ input.getPortCount();
 // Get the name of a specified input port.
 input.getPortName(0);
 
+var musician1_history = [];
+var musician2_history = [];
+var HISTORY_SIZE = 20;
+
 // Configure a callback.
 input.on('message', function(deltaTime, message) {
-	utils.log(message);
 	var mode = message[0];
 	var note = parseInt(message[1]);
 	var vel = message[2];
 
+	var d = new Date();
+
+	// in milliseconds
+	var timestamp = d.getTime();
+
 	if (note >= 60) {
-		utils.log("Musician 1 input: " + note);
+		utils.log("Musician 1 input: " + note + " at " + timestamp);
+		
+		if (musician1_history.length >= HISTORY_SIZE) {
+			musician1_history.pop();
+		}
+		musician1_history.unshift({'note': note, 'time': timestamp});
+		utils.log("Musician 1 history: " + JSON.stringify(musician1_history));
+
 	} else {
-		utils.log("Musician 2 input: " + note);
+		utils.log("Musician 2 input: " + note + " at " + timestamp);
+
+		if (musician2_history.length >= HISTORY_SIZE) {
+			musician2_history.pop();
+		}
+		musician2_history.unshift({'note': note, 'time': timestamp});
+
+		utils.log("Musician 2 history: " + JSON.stringify(musician2_history));
 	}
 
 });
